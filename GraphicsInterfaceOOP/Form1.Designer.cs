c65ac06ed02daf99db9fb372ca76945b230d32d8
opilane <opilane@HP-CZC6297BB7>
2021-11-10 15:43:00 +0200

    2nd part

--- GraphicsInterfaceOOP/Form1.Designer.cs
+++ GraphicsInterfaceOOP/Form1.Designer.cs
@@ -29,26 +29,32 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this.Run_Button = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textA = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.textC = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // Run_Button
             // 
-            this.button1.BackColor = System.Drawing.Color.SteelBlue;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
-            this.button1.FlatAppearance.BorderSize = 6;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.button1.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.button1.Location = new System.Drawing.Point(558, 24);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(191, 70);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Launch";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Run_Button.BackColor = System.Drawing.Color.SteelBlue;
+            this.Run_Button.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.Run_Button.FlatAppearance.BorderSize = 6;
+            this.Run_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Run_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Run_Button.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.Run_Button.Location = new System.Drawing.Point(558, 24);
+            this.Run_Button.Name = "Run_Button";
+            this.Run_Button.Size = new System.Drawing.Size(191, 70);
+            this.Run_Button.TabIndex = 0;
+            this.Run_Button.Text = "Launch";
+            this.Run_Button.UseVisualStyleBackColor = false;
+            this.Run_Button.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -60,27 +66,85 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(476, 187);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Column";
+            this.columnHeader1.Width = 218;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Value";
+            this.columnHeader2.Width = 276;
+            // 
+            // textA
+            // 
+            this.textA.Location = new System.Drawing.Point(358, 204);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(130, 20);
+            this.textA.TabIndex = 3;
+            this.textA.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textB
+            // 
+            this.textB.Location = new System.Drawing.Point(358, 230);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(130, 20);
+            this.textB.TabIndex = 4;
+            this.textB.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textC
+            // 
+            this.textC.Location = new System.Drawing.Point(358, 257);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(130, 20);
+            this.textC.TabIndex = 5;
+            this.textC.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.textC);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Run_Button);
             this.Cursor = System.Windows.Forms.Cursors.Cross;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Work with Triangle";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Run_Button;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox textC;
     }
 }
 
--- GraphicsInterfaceOOP/Form1.cs
+++ GraphicsInterfaceOOP/Form1.cs
@@ -19,6 +19,44 @@ namespace GraphicsInterfaceOOP
 
         private void button1_Click(object sender, EventArgs e)
         {
+            double a, b, c;
+            a = Convert.ToDouble(textA.Text);
+            b = Convert.ToDouble(textB.Text);
+            c = Convert.ToDouble(textC.Text);
+            Triangle triangle = new Triangle(a, b, c);
+            listView1.Items.Add("Side A");
+            listView1.Items.Add("Side B");
+            listView1.Items.Add("Side C");
+            listView1.Items.Add("Perimeter");
+            listView1.Items.Add("Surface Area");
+            listView1.Items.Add("Exists?");
+            listView1.Items.Add("Side A");
+            listView1.Items[0].SubItems.Add(triangle.outputA());
+            listView1.Items[1].SubItems.Add(triangle.outputB());
+            listView1.Items[2].SubItems.Add(triangle.outputC());
+            listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Perimeter()));
+            listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Surface()));
+            if (triangle.ExistTriange) { listView1.Items[5].SubItems.Add("Exists"); }
+            else listView1.Items[5].SubItems.Add("Doesn't exist");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
     }
